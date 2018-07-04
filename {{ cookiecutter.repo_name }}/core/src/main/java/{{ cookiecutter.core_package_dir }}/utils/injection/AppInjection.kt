ea3b545c255b47b340a926af595a382e3a463e95
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-04 23:04:56 +0200

    Added app injection component and module :

---
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/utils/injection/AppInjection.kt
@@ -0,0 +1,41 @@
+package {{ cookiecutter.core_package_name }}.utils.injection
+
+import android.app.Application
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import dagger.android.AndroidInjectionModule
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Singleton
+
+@Module
+class AppModule {
+
+    @Provides
+    fun context(app: Application): Context =
+            app
+
+    @Provides
+    fun compositeDisposable(): CompositeDisposable =
+            CompositeDisposable()
+}
+
+@Singleton
+@Component(modules = [
+    AndroidInjectionModule::class,
+    AppModule::class
+])
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun bindApplication(app: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(app: Application)
+}
