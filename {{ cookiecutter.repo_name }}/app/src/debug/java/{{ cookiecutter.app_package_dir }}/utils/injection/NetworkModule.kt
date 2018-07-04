9a33cd8d91ea275f803734cba08a35abf0b25817
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-05 01:25:32 +0200

    Removed unused imports

--- {{ cookiecutter.repo_name }}/app/src/debug/java/{{ cookiecutter.app_package_dir }}/utils/injection/NetworkModule.kt
+++ {{ cookiecutter.repo_name }}/app/src/debug/java/{{ cookiecutter.app_package_dir }}/utils/injection/NetworkModule.kt
@@ -1,12 +1,10 @@
 package {{ cookiecutter.app_package_name }}.utils.injection
 
-import android.content.Context
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import dagger.Module
 import dagger.Provides
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import javax.inject.Named
 import javax.inject.Singleton
 import {{ cookiecutter.core_package_name }}.utils.injection.BaseNetworkModule
 
