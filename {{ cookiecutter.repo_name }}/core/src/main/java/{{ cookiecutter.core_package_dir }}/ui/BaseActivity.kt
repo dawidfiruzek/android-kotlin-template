d2291448569006af178ef0ab021df91d32343b53
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-05 00:08:56 +0200

    Added base fragment and activity bodies

--- {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseActivity.kt
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseActivity.kt
@@ -1,6 +1,25 @@
 package {{ cookiecutter.core_package_name }}.ui
 
+import android.os.Bundle
+import android.support.annotation.CallSuper
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
-class BaseActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
+
+    @Inject lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> =
+            fragmentInjector
 }
--- {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseFragment.kt
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseFragment.kt
@@ -2,5 +2,16 @@ package {{ cookiecutter.core_package_name }}.ui
 
 import android.support.v4.app.Fragment
 
+import android.content.Context
+import android.support.annotation.CallSuper
+import android.support.v4.app.Fragment
+import dagger.android.support.AndroidSupportInjection
+
 class BaseFragment : Fragment() {
+
+    @CallSuper
+    override fun onAttach(context: Context?) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 }
