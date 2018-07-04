edfa77c5518d66b05d199673abcd2516a96a9fe6
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-04 22:39:09 +0200

    Added core elements

---
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.base_package_name |  replace('.', ':') }}:core/presentation/BaseActivity.kt
@@ -0,0 +1,6 @@
+package {{ cookiecutter.base_package_name }}.core.presentation
+
+import android.support.v7.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+}
---
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.base_package_name |  replace('.', ':') }}:core/presentation/BaseFragment.kt
@@ -0,0 +1,6 @@
+package {{ cookiecutter.base_package_name }}.core.presentation
+
+import android.support.v4.app.Fragment
+
+class BaseFragment : Fragment {
+}
