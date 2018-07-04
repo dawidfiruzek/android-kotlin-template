056b9ae6b8ee68fc9c8000698ee5200b15338d56
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-04 23:10:23 +0200

    Added injection to app

--- {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.app_package_dir }}/BaseApplication.kt
+++ {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.app_package_dir }}/BaseApplication.kt
@@ -2,9 +2,17 @@ package {{ cookiecutter.app_package_name }}
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import io.fabric.sdk.android.Fabric
+import {{ cookiecutter.core_package_name }}.utils.injection.DaggerAppComponent
+import javax.inject.Inject
 
-abstract class BaseApplication : Application() {
+abstract class BaseApplication : Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
@@ -13,4 +21,14 @@ abstract class BaseApplication : Application() {
     }
 
     abstract fun initTimber()
+
+    private fun initDaggerComponent() {
+        DaggerAppComponent.builder()
+                .bindApplication(this)
+                .build()
+                .inject(this)
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> =
+            activityInjector
 }
