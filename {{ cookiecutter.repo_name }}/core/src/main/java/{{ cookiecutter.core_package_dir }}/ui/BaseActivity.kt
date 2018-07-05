7be70842de95c315546f6bb5cf2e698f7890b288
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-05 10:53:28 +0200

    Added start activity method to base

--- {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseActivity.kt
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseActivity.kt
@@ -1,5 +1,7 @@
 package {{ cookiecutter.core_package_name }}.ui
 
+import android.content.Intent
+import android.os.Parcelable
 import android.os.Bundle
 import android.support.annotation.CallSuper
 import android.support.v4.app.Fragment
@@ -22,4 +24,15 @@ abstract class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> =
             fragmentInjector
+
+    fun <T : BaseActivity> startActivity(
+            activityClass: Class<T>,
+            flags: List<Int>? = null,
+            parcelable: Pair<String, Parcelable>? = null
+    ) {
+        val intent = Intent(this, activityClass)
+        flags?.forEach { intent.addFlags(it) }
+        parcelable?.let { intent.putExtra(it.first, it.second) }
+        startActivity(intent)
+    }
 }
