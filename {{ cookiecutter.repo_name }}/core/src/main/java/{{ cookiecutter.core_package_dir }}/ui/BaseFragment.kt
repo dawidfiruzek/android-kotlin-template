fc98f9b9a5c367bc30f2501ece11778c903df016
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-05 11:19:48 +0200

    Removed comments

--- {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseFragment.kt
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseFragment.kt
@@ -7,24 +7,11 @@ import dagger.android.support.AndroidSupportInjection
 
 abstract class BaseFragment : Fragment() {
 
-    // how to handle navigation in fragments in case of need
-    // 1. create listener property
-//    private lateinit var navigationListener: NavigationListener
-
     @CallSuper
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
-//        2. Set listener in onAttach method from Activity and implement NavigationListener there
-//        activity?.let {
-//            navigationListener = it as NavigationListener
-//        }
     }
-
-//    3. call listener's method in case of navigation
-//    fun handleNavigation(event: HomeNavigation) {
-//        navigationListener.handleEvent(event)
-//    }
 }
 
 interface NavigationEvent
@@ -32,8 +19,3 @@ interface NavigationEvent
 interface NavigationListener {
     fun handleEvent(event: NavigationEvent)
 }
-
-// e.g. navigation events specific to home
-// enum class HomeNavigation : NavigationEvent {
-//    PAY_CLICKED
-//}
