b8dc8baf050b626efca6f6920f121c1df109f4de
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-07-19 00:04:51 +0200

    Added base presenter activity

---
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BasePresenterActivity.kt
@@ -0,0 +1,19 @@
+package {{ cookiecutter.core_package_name }}.ui
+
+import {{ cookiecutter.core_package_name }}.BaseContract
+import javax.inject.Inject
+
+abstract class BasePresenterActivity<Presenter : BaseContract.Presenter> : BaseActivity(), HasSupportFragmentInjector {
+
+    @Inject lateinit var presenter: Presenter
+
+    override fun onStart() {
+        super.onStart()
+        presenter.initialize()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.clear()
+    }
+}
