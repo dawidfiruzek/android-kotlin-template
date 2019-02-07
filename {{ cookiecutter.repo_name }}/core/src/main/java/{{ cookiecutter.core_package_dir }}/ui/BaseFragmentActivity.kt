5511806ad29f28077dc2f00f7554c73e487328e0
Dawid Firuzek <dawid.firuzek@gmail.com>
2019-02-07 16:09:07 +0100

    Fixes

--- {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseFragmentActivity.kt
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/ui/BaseFragmentActivity.kt
@@ -1,5 +1,7 @@
 package {{ cookiecutter.core_package_name }}.ui
 
+import android.arch.lifecycle.ViewModel
+import android.databinding.ViewDataBinding
 import android.support.v4.app.Fragment
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
@@ -7,7 +9,7 @@ import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-abstract class BaseFragmentActivity : BaseActivity(), HasSupportFragmentInjector {
+abstract class BaseFragmentActivity<VM : ViewModel, DB: ViewDataBinding> : BaseActivity<VM, DB>(), HasSupportFragmentInjector {
 
     @Inject lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
 
