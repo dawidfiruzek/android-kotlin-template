23cec30c0a559b14367cda25444c6eb13f3c171e
Dawid Firuzek <dawid.firuzek@gmail.com>
2019-02-07 15:37:32 +0100

    Navigation module

--- {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/utils/injection/ResourcesModule.kt
+++ {{ cookiecutter.repo_name }}/core/src/main/java/{{ cookiecutter.core_package_dir }}/utils/injection/ResourcesModule.kt
@@ -3,13 +3,12 @@ package {{ cookiecutter.core_package_name }}.utils.injection
 import android.content.Context
 import {{ cookiecutter.core_package_name }}.utils.resources.ResourceProvider
 import {{ cookiecutter.core_package_name }}.utils.resources.ResourceProviderImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
-class ResourcesModule {
+abstract class ResourcesModule {
 
-    @Provides
-    fun resourceProvider(context: Context): ResourceProvider =
-            ResourceProviderImpl(context)
+    @Binds
+    abstract fun resourceProvider(impl: ResourceProviderImpl): ResourceProvider
 }
